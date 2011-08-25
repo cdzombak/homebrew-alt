cf2a47cb386eae02c448477c47f149e29cc809f1
Chris Dzombak <chris@chrisdzombak.net>
2011-08-25 14:11:09 -0400

    fix nano build to function the same as on my linux machines

--- duplicates/nano.rb
+++ duplicates/nano.rb
@@ -5,18 +5,10 @@ class Nano < Formula
   homepage 'http://www.nano-editor.org/'
   md5 '03233ae480689a008eb98feb1b599807'
 
-  depends_on "s-lang"
-
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--enable-color",
-                          "--enable-extra",
-                          "--enable-multibuffer",
-                          "--enable-nanorc",
-                          "--disable-nls",
-                          "--enable-utf8",
-                          "--with-slang=#{HOMEBREW_PREFIX}"
+                          "--enable-all"
     system "make install"
   end
 end
